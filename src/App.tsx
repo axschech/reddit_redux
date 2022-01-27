ab878c03aade306c30be5d2d4a7d2ba98871e524
Alex Schechter <alexschechter@Alexs-MacBook-Air.local>
2022-01-26 22:38:00 -0500

    Adding title

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import { Images } from './features/images/Images';
 function App() {
   return (
     <div>
+      <h1>Reddit Redux</h1>
+      <div>
+        <a href='https://github.com/axschech/reddit_redux.git'>Github</a>
+      </div>
       <Images></Images>
     </div>
   );
